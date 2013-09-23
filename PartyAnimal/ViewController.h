dc803bbe94239a1f2bc5ed4bef94ba29dc713ca3
Adrian Florian <adrianflorian@me.com>
2013-09-23 15:38:09 +0300

    Initial Commit

---
+++ PartyAnimal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PartyAnimal
+//
+//  Created by Gina Lupu on 9/23/13.
+//  Copyright (c) 2013 RubyTribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PartyAnimal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PartyAnimal
+//
+//  Created by Gina Lupu on 9/23/13.
+//  Copyright (c) 2013 RubyTribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
